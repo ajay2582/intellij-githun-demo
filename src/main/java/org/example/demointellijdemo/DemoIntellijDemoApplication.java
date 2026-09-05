f71e6589b9d8e1f8eee631e908fd0dd8ebfa9a87
rajputajay2582 <Ajay.kumar@0101digitall.com>
2026-09-05 12:09:44 +0530

    initiate the commit and do the changes in the demo

--- src/main/java/org/example/demointellijdemo/DemoIntellijDemoApplication.java
+++ src/main/java/org/example/demointellijdemo/DemoIntellijDemoApplication.java
@@ -9,5 +9,5 @@ public class DemoIntellijDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoIntellijDemoApplication.class, args);
     }
-
+// commented line
 }
