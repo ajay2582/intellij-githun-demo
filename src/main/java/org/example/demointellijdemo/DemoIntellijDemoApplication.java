6bd77f84f97b186dfe7e5e01655bf10765df3658
rajputajay2582 <Ajay.kumar@0101digitall.com>
2026-09-05 12:07:58 +0530

    initiate the commit

---
+++ src/main/java/org/example/demointellijdemo/DemoIntellijDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.demointellijdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIntellijDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoIntellijDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demointellijdemo/DemoIntellijDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demointellijdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIntellijDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
